a86c68126ad12e4d928815f255cd0f85c48c59ad
David Jaimes <davidjaimesdesign@gmail.com>
2016-12-18 07:06:46 -0800

    finished chapter on basic api calls moving on to pagination

--- src/App.js
+++ src/App.js
@@ -3,34 +3,36 @@ import './App.css';
 import './index.css';
 
 const DEFAULT_QUERY = 'redux'
-const PATH_BASE     = 'https://hn.algolia.com/api/vi';
+const PATH_BASE     = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH   = '/search';
 const PARAM_SEARCH  = 'query='; 
-
-const isSearched = (query) => (item) => !query || item.title.toLowerCase().indexOf(query.toLowerCase()) !== -1;
-
-const list = [
-];
 
 class App extends Component {
   constructor(props) {
    super(props);
 
    this.state = {
-     list,
+     result: null,
      query: DEFAULT_QUERY,
    };
    this.setSearchTopstories = this.setSearchTopstories.bind(this);
    this.fetchSearchTopstories = this.fetchSearchTopstories.bind(this);
    this.onSearchChange = this.onSearchChange.bind(this);
+   this.onSearchSubmit = this.onSearchSubmit.bind(this);
   }
   
+  onSearchSubmit (event){
+   const {query} = this.state;
+   this.fetchSearchTopstories(query);
+   event.preventDefault();
+  }
+
   setSearchTopstories(result) {
     this.setState({result});
   }
 
   fetchSearchTopstories(query) {
-    fetch('${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${query}')
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${query}`)
       .then(response => response.json())
       .then(result => this.setSearchTopstories(result));
   }
@@ -45,27 +47,28 @@ class App extends Component {
   }
   
 render() {
-  const { query, list } = this.state;
+  const { query, result} = this.state;
   return (
     <div className="page">
       <div className="interactions">
-        <Search value={query} onChange={this.onSearchChange}>
+        <Search value={query} onChange={this.onSearchChange} onSubmit={this.onSearchSubmit}>
           Search
         </Search>
       </div>
-      <Table list={list} pattern={query} />
+      {result && <Table list={result.hits}/>}
     </div>
   );
   }
 }
-const Search = ({ value, onChange, children }) =>
-  <form>
-    {children} <input type="text" value={value} onChange={onChange} />
+const Search = ({ value, onChange, onSubmit, children }) =>
+  <form onSubmit={onSubmit}>
+    <input type="text" value={value} onChange={onChange} />
+    <button type="submit">{children}</button>
   </form>
 
-const Table = ({ list, pattern }) =>
+const Table = ({ list }) =>
   <div className="table">
-    { list.filter(isSearched(pattern)).map((item) =>
+    { list.map((item) =>
       <div key={item.objectID} className="table-row">
         <span style={{width: '40%'}}>
           <a href={item.url}>{item.title}</a>
