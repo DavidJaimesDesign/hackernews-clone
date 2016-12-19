1f92fda3a860d3eec8c12f11dbbe451fba3ca830
David Jaimes <davidjaimesdesign@gmail.com>
2016-12-19 08:33:46 -0800

    Working on the search cache

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import './App.css';
 import './index.css';
 
 const DEFAULT_QUERY = 'redux'
+const DEFAULT_PAGE  = 0;
+const DEFAULT_HPP   = 100;
 const PATH_BASE     = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH   = '/search';
 const PARAM_SEARCH  = 'query='; 
-
+const PARAM_PAGE    = 'page=';
+const PARAM_HPP     = 'hitsPerPage=';
 class App extends Component {
   constructor(props) {
    super(props);
 
    this.state = {
-     result: null,
+     results: null,
      query: DEFAULT_QUERY,
    };
    this.setSearchTopstories = this.setSearchTopstories.bind(this);
@@ -23,31 +26,40 @@ class App extends Component {
   
   onSearchSubmit (event){
    const {query} = this.state;
-   this.fetchSearchTopstories(query);
-   event.preventDefault();
+   this.fetchSearchTopstories(query,DEFAULT_PAGE); 
+   event.preventDefault(); 
   }
 
-  setSearchTopstories(result) {
-    this.setState({result});
-  }
-
-  fetchSearchTopstories(query) {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${query}`)
+  fetchSearchTopstories(query, page){
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${query}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
       .then(response => response.json())
       .then(result => this.setSearchTopstories(result));
   }
 
   componentDidMount() {
     const{query} = this.state;
-    this.fetchSearchTopstories(query);
+    this.fetchSearchTopstories(query, DEFAULT_PAGE);
   }
 
   onSearchChange(event) {
     this.setState({ query: event.target.value});
   }
   
+  setSearchTopstories(result) {
+    const {hits, page} = result
+    const { query }    = this.state;
+ 
+    const oldHits = page === 0 ? [] : this.state.results[query].hits;
+    const updatedHits = [ ...oldHits, ...hits];
+
+    this.setState(
+      { result: { ...this.state.results, [query]: {hits: updatedHits, page}}
+   });
+  }  
 render() {
-  const { query, result} = this.state;
+  const { query, results} = this.state;
+  const page = (results && results[query] && results[query].page) || 0;
+  const list = (results && results[query] && results[query].hits) || [];
   return (
     <div className="page">
       <div className="interactions">
@@ -55,7 +67,12 @@ render() {
           Search
         </Search>
       </div>
-      {result && <Table list={result.hits}/>}
+      <Table list={list}/>
+      <div>
+        <Button onClick={() => this.fetchSearchTopstories(query, page + 1)}>
+          More
+        </Button>
+      </div>
     </div>
   );
   }
@@ -86,4 +103,8 @@ const Table = ({ list }) =>
   )}
   </div>
 
+const Button = ({ onClick, children}) =>
+  <button onClick={onClick} type="button">
+    {children}
+  </button>
 export default App;
